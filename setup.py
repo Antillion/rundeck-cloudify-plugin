20e7083ecbc591c804415029147b6a091531bbdd
Oliver Tupman <oliver@dts-workshop.com>
2016-04-08 10:32:05 +0100

    Includes and renames workflow declarations in the plugin (to where they should be). Also updates the README and bumps the version number.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 setup(
     name='cloudify-rundeck-plugin',
 
-    version='0.1.1',
+    version='0.1.2',
     author='Oliver Tupman',
     author_email='otupman@antillion.com',
     description='Rundeck Plugin for Cloudify',

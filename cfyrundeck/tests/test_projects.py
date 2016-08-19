506b2e259f73269725dc328310b1609c61e61025
Oliver Tupman <oliver@dts-workshop.com>
2016-08-19 12:33:27 +0100

    Refactor to remove duplication

--- cfyrundeck/tests/test_projects.py
+++ cfyrundeck/tests/test_projects.py
@@ -19,7 +19,7 @@ import os
 import unittest
 import importlib
 
-from mock import Mock, MagicMock, PropertyMock
+
 from mock import patch
 
 from cloudify.workflows import local
@@ -29,7 +29,12 @@ sys.path.append(os.getcwd() + '/cfyrundeck')
 
 import cfyrundeck.jobs as jobs
 import cfyrundeck.projects as projects
+from cloudify.workflows import local
 from rundeck.client import Rundeck
+
+from cloudify.state import current_ctx
+
+from . import PluginTestBase
 
 #
 # Enable mocking of importlib.import_module so we can intercept calls to
@@ -53,16 +58,11 @@ def get_blueprint_path():
   return os.getcwd() + '/cfyrundeck/tests/blueprint'
 
 
-class TestRundeckPlugin(unittest.TestCase):
-
-  def setup_get_mock(self, GetMock, response_status, response_data):
-    GetMock.return_value = MagicMock()
-    GetMock.return_value.__str__.return_value = response_data
-    type(GetMock.return_value).status_code = PropertyMock(return_value=response_status)
-    type(GetMock.return_value).text = PropertyMock(return_value=response_data)
-    type(GetMock.return_value).content = PropertyMock(return_value=response_data)
-
+class TestProjectsOperation(PluginTestBase):
   def test_import_archive(self):
+    ctx = self.get_mock_context('test_import_archive')
+    current_ctx.set(ctx=ctx)
+
     with patch('cfyrundeck.utils.Rundeck') as RundeckMock, patch('cfyrundeck.projects.get') as GetMock:#, open('{0}/{1}'.format(get_blueprint_path(), job_filename), 'r') as blueprint_file:
       instance = RundeckMock.return_value
       self.setup_get_mock(GetMock, 200, 'test')
@@ -79,11 +79,3 @@ class TestRundeckPlugin(unittest.TestCase):
       instance.import_project_archive.assert_called_once_with(
         workflow_parameters['project'], 'test'
       )
-
-  def setUp(self):
-    blueprint_path = os.path.join(os.path.dirname(__file__),
-                                  'blueprint', 'test_rundeck.yaml')
-    inputs = {}
-    self.env = local.init_env(blueprint_path,
-                              name=self._testMethodName,
-                              inputs=inputs)

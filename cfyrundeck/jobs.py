f4a6d0ce668696df87c9424fd73345f61d460efa
Oliver Tupman <otupman@dts-workshop.com>
2016-12-12 12:26:30 +0000

    Quick bit of tidying up of unused code and ordering of imports

--- cfyrundeck/jobs.py
+++ cfyrundeck/jobs.py
@@ -13,68 +13,21 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
-# ctx is imported and used in operations
+import utils
+import time
+
 from cloudify import ctx
-from cloudify.logs import send_task_event
-# put the operation decorator on any function that is a task
 from cloudify.decorators import operation, workflow
 from cloudify.exceptions import NonRecoverableError, RecoverableError
 
-import utils
-import time
 from requests import get, codes
-
-def search_tree_for_property(curr_node_instance, property_name, default_val=None):
-    assert curr_node_instance is not None
-    assert property_name is not None
-
-    if property_name in curr_node_instance.runtime_properties:
-        return curr_node_instance.runtime_properties[property_name]
-
-    bp_node = curr_node_instance._node
-
-    if property_name in bp_node.properties:
-        return bp_node.properties[property_name]
-
-    if len(curr_node_instance.relationships) is 0:
-        return default_val
-
-    def for_contained_in(relationship): return relationship.type == 'cloudify.relationships.contained_in'
-
-    contained_in_rels = filter(for_contained_in, curr_node_instance.relationships)
-
-    if len(contained_in_rels) is 0:
-        return default_val
-
-    assert len(contained_in_rels) is 1, 'Nodes can only be contained in exactly 1 other node.'
-
-    return search_tree_for_property(contained_in_rels[0].target.instance, property_name, default_val)
-
 
 
 @operation
 def execute(**kwargs):
-    rundeck = utils.create_rundeck_client(kwargs)
-    job_id = kwargs['job_id']
-    poll_in_s = kwargs['poll_in_s'] if kwargs.has_key('poll_in_s') else 10
+    kwargs['rundeck_config'] = kwargs
+    execute_v2(**kwargs)
 
-    ctx.logger.info("Rundeck[{0}]: Starting`...".format(job_id))
-    run_result = rundeck.run_job(job_id, argString=kwargs['args'])
-
-    execution_id = run_result['id']
-    ctx.logger.info("Rundeck[{0}]:Execution[{1}] Started...".format(job_id, execution_id))
-
-    status = 'running'
-    while status == 'running':
-        time.sleep(poll_in_s)
-        execution_status = rundeck.execution_status(execution_id)
-        ctx.logger.info("Rundeck[{0}]:Execution[{1}] Checked execution, status is: {2}".
-                        format(job_id, execution_id, execution_status['status']))
-        status = execution_status['status']
-
-    ctx.logger.info("Rundeck[{0}]:Execution[{1}] {2} ".format(job_id, execution_id, status))
-    if status != 'succeeded':
-        raise NonRecoverableError("Execution [{0}] did not succeed, result was: {1}".format(execution_id, status))
 
 @operation
 def execute_v2(**kwargs):
